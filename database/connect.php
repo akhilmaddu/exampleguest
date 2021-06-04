4d4b42c185059db5c89be7c400458103f8f6a440
akhilmaddu <84220462+akhilmaddu@users.noreply.github.com>
2021-06-04 10:58:28 +0530

    Update connect.php

--- database/connect.php
+++ database/connect.php
@@ -1,14 +1,33 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = FALSE;
-$dbname = "akhil";
+<?php 
+		if (function_exists('conectar')){
 
-//Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+		} else {
 
-//Check connection
-if(!$conn){
-	die("Connection failed: ".mysqli_connect_error);
-}
+		function conectar(){
+		   
+				//$connection = mysqli_connect("localhost", 'root', '', 'cl_water_sys');	
+
+				$user = getenv('CLOUDSQL_USER');
+				$pass = getenv('CLOUDSQL_PASSWORD');
+				$inst = getenv('CLOUDSQL_DSN');
+				$db = getenv('CLOUDSQL_DB');
+				$connection = mysqli_connect(null, $user, $pass, $db, null, $inst);
+			/*	$now = new DateTime();
+				$mins = $now->getOffset() / 60;
+				$sgn = ($mins < 0 ? -1 : 1);
+				$mins = abs($mins);
+				$hrs = floor($mins / 60);
+				$mins -= $hrs * 60;
+				$offset = sprintf('%+d:%02d', $hrs*$sgn, $mins);
+				$connection->query("SET time_zone='$offset'");
+			    $connection->query("SET lc_time_names = 'es_ES'"); */
+				if (!$connection) {
+						echo "Error!".mysqli_connect_error();
+					}
+				
+				return $connection;
+		}
+		}
+
+		
 ?>
